338b7b01f1fcc1a7ad0acb6d286bac39a25819c8
rreisnascimento <roger088reis@gmail.com>
2022-09-19 16:39:09 -0300

    Console log no POST PreCadastro

--- src/controllers/PreCadastroProfissionalController.js
+++ src/controllers/PreCadastroProfissionalController.js
@@ -2,6 +2,7 @@ const pgSQL = require("../config/dbConnectionPG");
 const adfunc = require("../utils/utilsfunc");
 
 const create = async (req, res) => {
+    console.log("Dados chegando: " + req.body);
     return res.status(200).json({ message: req.body });
     // const sql = `insert into precadastro 
     //                 (uuid, nome, especialidade, endereco, complemento, bairro, cidade, uf, created, aceito) 
