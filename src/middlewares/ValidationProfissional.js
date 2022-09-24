f8309cd3988acd021c275dcf128963577ffbdb1a
rreisnascimento <roger088reis@gmail.com>
2022-09-24 18:09:13 -0300

    Atualizando validação de Especialidade do Profissional

--- src/middlewares/ValidationProfissional.js
+++ src/middlewares/ValidationProfissional.js
@@ -6,33 +6,16 @@ const ValidationProfissional = (req, res, next) => {
             status: 400,
             message: "Nome do Profissional Obrigatório!" 
         })
+    else if (!especialidade)
+        return res.status(400).json({ 
+            status: 400,
+            message: "Especialidade do Profissional Obrigatório!" 
+        })
     else if (!endereco)
         return res.status(400).json({ 
             status: 400,
             message: "Endereço do Profissional Obrigatório!" 
         })
-    // else if (!clinica)
-    //     return res.status(400).json({ message: "Nome da Clinica Obrigatório!" })
-    // else if (!especialidade)
-    //     return res.status(400).json({ message: "Especialidade do Profissional Obrigatório!" })
-    // else if (!cep)
-    //     return res.status(400).json({ message: "CEP é Obrigatório!" })        
-    // else if (!endereco)
-    //     return res.status(400).json({ message: "Endereço do Profissional Obrigatório!" })
-    // else if (!complemento)
-    //     return res.status(400).json({ message: "Complemento do Profissional Obrigatório!" })
-    // else if (!bairro)
-    //     return res.status(400).json({ message: "Bairro do Profissional Obrigatório!" })
-    // else if (!cidade)
-    //     return res.status(400).json({ message: "Cidade do Profissional Obrigatório!" })
-    // else if (!uf)
-    //     return res.status(400).json({ message: "UF do Profissional Obrigatório!" })
-    // else if (!comercial)
-    //     return res.status(400).json({ message: "Telefone Comercial é Obrigatório!" })
-    // else if (!celular)
-    //     return res.status(400).json({ message: "Telefone Celular é Obrigatório!" })
-    // else if (!email)
-    //     return res.status(400).json({ message: "Necessário informar um Email válido!" })
     else next();
 }
 
